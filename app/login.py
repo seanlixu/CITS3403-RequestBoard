632ef42aab19ae6829bdb70a26900e43f05d17de
aidan <aidenpower03@gmail.com>
2024-04-18 11:46:03 +0800

    Changed render_template to jsonify for efficiency, added routes.py

--- app/login.py
+++ app/login.py
@@ -18,7 +18,7 @@ login_manager = LoginManager()
 # Create Bcrypt object with app as param
 bcrypt = Bcrypt(app)
 
-@app.route('/login/<username>, <password>', methods=['POST'])
+@app.route('/login/<username>, <password>', methods=['GET', 'POST'])
 def login():
     # Get data from input
     data = request.json
@@ -28,26 +28,26 @@ def login():
     # data = {'username': 'user6', 'password': 'password9'}
     # user = "user5"
     # password = "pw2"
-    user = data.get('username')
-    password = data.get('password')
-
     # cur.execute("SELECT * FROM user")
     # rows = cur.fetchall()
     # for row in rows:
     #     print(row)
 
-    # Error variable for frontend access
-    status = ""
 
-    # Check if user or password are empty
-    if not user or not password:
-        status = "User or Password not found"
-        return render_template('login.html', status=status)
-        
-    
+    # User and password variables from data
     user = data.get('username')
     password = data.get('password')
 
+    # response variable for frontend access
+    response = {'status': '', 'message': ''}
+    # Check if user or password are empty
+    if not user or not password:
+        response['status'] = 'error'
+        response['message'] = "Username or password not found"
+        return jsonify(response), 400
+        
+        
+    
     # Select pw where username matches and if nothing return error
     cur.execute("SELECT password FROM user WHERE username=?", (user,))
     # Rename variable. Password tuple. Fetches data of password column
@@ -55,93 +55,20 @@ def login():
 
     # Check if any results returned
     if not current_user:
-        status = "Username or Password not found"
-        return render_template('login.html', status=status)
+        response['status'] = 'error'
+        response['message'] = "Username or Password not found"
+        return jsonify({'status': 'error', 'message': status}), 404
+        
     # Check if hashed password matches input password
     if bcrypt.check_password_hash(current_user[0], password):
-        status = "None"
+        response['status'] = 'success'
+        response['message'] = "Login successful"
         return redirect('/home')
     else: 
         status = "Incorrect Password, try again"
-        return render_template('login.html', status=status)
-
-@app.route('/home')
-def render_home():
-    return render_template('home.html')
-
+        response['status'] = 'error'
+        response['message'] = "Incorrect Password, try again"
+        return jsonify(response), 401
+        
 login()
 
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
-# from flask import Flask, request, jsonify
-# from flask_login import LoginManager
-# from flask_bcrypt import Bcrypt
-# import sqlite3
-
-# app = Flask(__name__)
-# # https://docs.python.org/3/library/sqlite3.html
-# # Connect to users.db
-# con = sqlite3.connect("users.db")
-
-# # Make connection for executing SQL queries
-# cur = con.cursor()
-
-# # Create login manager
-# login_manager = LoginManager()
-
-# # Create Bcrypt object with app as param
-# bcrypt = Bcrypt(app)
-# def valid_user(user, password):
-#     # Check if user is empty or password empty
-#     if not user or not password:
-#         return False, "User or password not found"
-    
-#     # Select pw where username matches, if nothing return error
-#     cur.execute("SELECT password FROM user WHERE username=?", (user,))
-#     # Fetch data of password column
-#     current_user = cur.fetchone()
-#     # Check if results returned
-#     if not current_user:
-#         return False, "Incorrect username or password"
-    
-#     if bcrypt.check_password_hash(current_user[0], password):
-#         return True, "Login successful"
-    
-#     else: return False, "Incorrect username or password"
-
-# @app.route('/login', methods=['POST'])
-# def login():
-#     # Get data from input
-#     # data = request.json
-
-#     # Testing
-#     data = {'username': 'user5', 'password': 'pw3'}
-#     user = "user5"
-#     password = "pw2"
-
-#     # Check if user or password are empty
-#     if not user or not password:
-#         return "User or password not found", 400
-    
-#     user = data.get('username')
-#     password = data.get('password')
-
-#     is_valid, message = valid_user(user, password)
-
-#     if is_valid:
-#         return message, 200
-    
-#     else:
-#         return message, 400
-
-        
-# # Create function that validates the user and revamp this code.
-
-# # Maybe add more functions like showHomePage and setupAccount
-
-
-# login()
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
 from app import app
-g
+from flask import render_template
+from .login import login
+from .register import register_user
+
+@app.route('/login', methods=['POST'])
+def login_route():
+    return login()
+
+@app.route('/register', methods=['POST'])
+def register_route():
+    return register_user()
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
