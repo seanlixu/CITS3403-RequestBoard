3cb2927affa508ffc312623da4ae1e82867153a1
Paul M <paulmaingistejoy@gmail.com>
2024-05-07 22:45:48 +0800

    reconfigured some files (see for more details) Added a .env file for environment variables, also added it to gitignore Redid the config file with multiple classes for development, testing and production Added a LICENSE, just a standard MIT license file

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'users.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "not-very-secret"
-    # SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
