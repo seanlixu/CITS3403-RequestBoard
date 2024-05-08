4f3a083ee90541ce172e829b97722b2ead76d563
seanlixu <weihaolixu@gmail.com>
2024-05-08 14:28:17 +0800

    organised files and minor ui fixes

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("")
+
+@app.route("/userdashboard")
+def userDashboard():
+    return render_template("userDashboard.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
