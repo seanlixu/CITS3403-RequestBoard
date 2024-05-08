0577833c874568f8c8c6c7a7a5671be648b41514
Paul M <paulmaingistejoy@gmail.com>
2024-05-08 21:52:47 +0800

    removed the cli command from the code db can be initialised easily with `flask db init` and tables created using a migrate so dont see the need to have this cli command

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,19 @@
-# Imports
 from flask import Flask
-from .config import Config
+from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
-# Flask app name
 flaskApp = Flask(__name__)
 
-# Setting config
 flaskApp.config.from_object(Config)
 
-# Set db to SQL of flaskapp
-db = SQLAlchemy(flaskApp)
+login = LoginManager(flaskApp)
+login.login_view = 'login'
 
+db = SQLAlchemy(flaskApp)
 migrate = Migrate(flaskApp, db)
 
 
-@flaskApp.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    print("created")
-    db.create_all()
-
-# Importing models and routes
 from app import models, routes
