1042463e6144761ee36ae115ead424d183f81bab
Paul M <paulmaingistejoy@gmail.com>
2024-05-08 23:44:01 +0800

    hashing passwords in test data lol

--- app/test_data.py
+++ app/test_data.py
@@ -1,11 +1,12 @@
 from app import db
 from app.models import *
+from werkzeug.security import generate_password_hash
 
 
-john = User(username='John', email='john@john.com', password='Password1')
-jane = User(username='Jane', email='jane@jane.com', password='Password2')
-bob = User(username='Bob', email='bob@bob.com', password='Password3')
-alice = User(username='Alice', email='alice@alice.com', password='Password4')
+john = User(username='John', email='john@john.com', password=generate_password_hash('Password1'))
+jane = User(username='Jane', email='jane@jane.com', password=generate_password_hash('Password2'))
+bob = User(username='Bob', email='bob@bob.com', password=generate_password_hash('Password3'))
+alice = User(username='Alice', email='alice@alice.com', password=generate_password_hash('Password4'))
 
 post1 = Post(title='John post 1', content='Iamjohn', author=john)
 post2 = Post(title='John post 2', content='Iamjohn', author=john)
