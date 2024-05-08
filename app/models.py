a2be9bb32658612587a0270c1af392950557ef19
Paul M <paulmaingistejoy@gmail.com>
2024-05-08 21:41:25 +0800

    Added database model for posts Also added some new fields  to accomodate email signup and modified some field capacities e.g password to store the hashed pwd deleted the db file on repo, since using sqlalchemy we can just maintain local copy, have .db in gitignore generated a couple of migration scripts since was testing/modifying the fields

--- app/models.py
+++ app/models.py
@@ -1,16 +1,45 @@
-from app import db
-# from sqlalchemy.orm import relationship
+from app import db, login
+from datetime import datetime, timezone
+from werkzeug.security import check_password_hash
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    # indexing the username and email columns for faster search since will be used for login
+    username = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True, nullable=False, index=True)
+    # to store profile picture
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(256), nullable=False)
     ratings = db.Column(db.Float(5.0), default=0.0)
     ratings_count = db.Column(db.Integer(), default=0)
+    
+    posts = db.relationship('Post', back_populates='author')
+    # posts = db.relationship('Post', back_populates='author', lazy='dynamic')
+    
+    def __repr__(self):
+        return f'<User {self.username}, {self.email}, {self.image_file}>'
 
-    # posts = relationship('Post', backref='author', lazy='dyanmic')
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc), index=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, index=True)
+    
+    author = db.relationship('User', back_populates='posts')
+    # author = db.relationship('User', back_populates='posts', lazy='dynamic')
+    def __repr__(self):
+        return f'<Post {self.title}, {self.timestamp}>'
 
     
 class Response:
